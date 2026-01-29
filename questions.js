05a6e00fc00ca55e925ea24a4dfb0d09b45e7217
jangird <jangird581@gmail.com>
2026-01-29 17:25:15 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,18 @@
+const questions = [
+  {
+    question: "Rajasthan ka rajya pakshi kya hai?",
+    options: ["Mor", "Godawan", "Kabutar", "Koyal"],
+    answer: "Godawan"
+  },
+  {
+    question: "Rajasthan ki rajdhani kya hai?",
+    options: ["Jodhpur", "Jaipur", "Udaipur", "Kota"],
+    answer: "Jaipur"
+  },
+  {
+    question: "REET परीक्षा किस राज्य में होती है?",
+    options: ["MP", "UP", "Rajasthan", "Bihar"],
+    answer: "Rajasthan"
+  }
+];
+
